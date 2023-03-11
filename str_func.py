2c2d71cbb3e0d9deb19cec3119ea8d8f26b658f6
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-03-11 23:11:12 +0300

    edit str

--- str_func.py
+++ str_func.py
@@ -1,2 +1,26 @@
 def str_upper(text):
+    """
+    переводит текст в верхний регистр
+    :param text:
+    :return:
+    """
     return text.upper()
+
+
+def str_title(text):
+    """
+    Каждое слово с большой буквы
+    :param text: any text
+    :return: text title
+    """
+    return text.title()
+
+
+def str_lower(text):
+    """
+    Каждое слово с большой буквы
+    :param text: any text
+    :return: text title
+    """
+    return text.lower()
+
